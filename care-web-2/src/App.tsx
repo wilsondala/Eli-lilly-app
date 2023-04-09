bf4750a24182e77da31edc69badc53c63818f11e
Wilson dos Santos Kahango Dala <wilson.dala@viveo.com.br>
2023-04-08 23:58:08 -0300

    'configuracao'

---
+++ care-web-2/src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+
+
+export const App = () => {
+  return (
+    <BrowserRouter>
+  <AppRoutes />
+    </BrowserRouter>
+  );
+}
+
